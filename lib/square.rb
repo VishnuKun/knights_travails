6759e5e6d52b974f7ab8bb65b0773b2d61d3dfc7
Vishnu <vishnukun954AV@protonmail.com>
2023-08-06 19:15:10 +0530

    Added distance attribute for bfs traversal default value set to infinity.

--- lib/square.rb
+++ lib/square.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Square
-  attr_accessor :coordinate, :neighbours, :parent, :visited
+  attr_accessor :coordinate, :neighbours, :parent, :visited, :distance
 
   def initialize(coordinate = nil)
     @coordinate = coordinate
     @neighbours = []
     @parent = nil
     @visited = false
+    @distance = Float::INFINITY
   end
 end
